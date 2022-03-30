118c97383410a31829a175951b1b1d189dbd6303
angelo <madhav1004@icloud.com>
2022-03-30 18:20:48 +0530

    complete layout

--- Components/Home/index.jsx
+++ Components/Home/index.jsx
@@ -1,10 +1,64 @@
-import React from "react";
-import { Container } from "./Home.style";
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  ContentContainer,
+  DateContainer,
+  TextContainer,
+  TitleContainer,
+} from "./Home.style";
+import { format } from "date-fns";
 
 function Home() {
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    setCurrentDate(format(new Date(), "PPP"));
+  }, []);
+
   return (
     <Container>
-      <span>Home</span>
+      <DateContainer>
+        <span>{currentDate}</span>
+      </DateContainer>
+      <ContentContainer>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+      </ContentContainer>
+      <TitleContainer>
+        <span>Developer</span>
+      </TitleContainer>
     </Container>
   );
 }
